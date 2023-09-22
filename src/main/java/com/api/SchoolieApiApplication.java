f7d22edf89d7f6dcb81b1e6ddb7275cd651a3acc
Gabriel Pickler <gpickler98@gmail.com>
2023-09-21 21:24:18 -0300

    fix(SchoolieApiApplication): remove componentScan annotation

--- src/main/java/com/api/SchoolieApiApplication.java
+++ src/main/java/com/api/SchoolieApiApplication.java
@@ -3,9 +3,7 @@ package com.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class SchoolieApiApplication {
 
