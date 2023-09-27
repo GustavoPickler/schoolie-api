36082b8f5ed4777ac50577c3141829fcd006e520
Gabriel Pickler <gpickler98@gmail.com>
2023-09-27 18:01:35 -0300

    feat(AuthController): coloca logica de login no service

--- src/main/java/com/api/auth/controller/AuthController.java
+++ src/main/java/com/api/auth/controller/AuthController.java
@@ -2,14 +2,10 @@ package com.api.auth.controller;
 
 import com.api.auth.dto.request.LoginRequestDTO;
 import com.api.auth.dto.response.LoginResponseDTO;
-import com.api.auth.exceptions.AuthException;
 import com.api.auth.service.AuthService;
-import com.api.auth.util.JwtGenerator;
 import com.api.users.exception.NotFoundException;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
@@ -30,16 +26,9 @@ import javax.validation.Valid;
 @Slf4j
 @RestController
 @RequestMapping("/api/auth")
-@SecurityScheme(
-        name = "JWT Bearer Token",
-        type = SecuritySchemeType.HTTP,
-        bearerFormat = "JWT",
-        scheme = "bearer"
-)
 public class AuthController {
 
     private final AuthService service;
-    private final JwtGenerator jwtGenerator;
 
     @Operation(
             summary = "Autenticar Usuário",
@@ -49,23 +38,13 @@ public class AuthController {
                     @ApiResponse(responseCode = "200", description = "Autenticação bem-sucedida"),
                     @ApiResponse(responseCode = "401", description = "Credenciais inválidas"),
                     @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
-            }
-    )
+            })
     @PostMapping(
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE
     )
     public LoginResponseDTO authenticateUser(@Valid @RequestBody LoginRequestDTO authRequest) throws NotFoundException {
-        String email = authRequest.getEmail();
-        String password = authRequest.getPassword();
-
-        boolean authenticationSuccessful = service.authenticate(email, password);
-
-        if (authenticationSuccessful) {
-            String authToken = jwtGenerator.generateToken(email);
-            return new LoginResponseDTO(authToken);
-        } else {
-            throw new AuthException("teste", "Authentication failed");
-        }
+        final String token = service.authenticate(authRequest);
+        return new LoginResponseDTO(token);
     }
 }
