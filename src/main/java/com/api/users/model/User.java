06e9873dfc75a9d886e46565e89fae36f8f30df5
Gabriel Pickler <gpickler98@gmail.com>
2023-09-27 18:15:46 -0300

    feat(User): implementa UserDetails, ajusta birthDate para LocalDate e adiciona coluna active

--- src/main/java/com/api/users/model/User.java
+++ src/main/java/com/api/users/model/User.java
@@ -2,15 +2,20 @@ package com.api.users.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.Date;
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Data
 @Table(name = "users")
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "user_type")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -27,9 +32,42 @@ public class User {
     @Column(nullable = false)
     private String phone;
 
-    @Column(name = "user_type", insertable = false, updatable = false)
-    private String userType;
-
     @Column(name = "birth_date")
-    private Date birthDate;
+    private LocalDate birthDate;
+
+    @Column(name = "user_type", insertable = false, updatable = false)
+    @Enumerated(EnumType.STRING)
+    private UserType userType;
+
+    @Column(name = "active")
+    private boolean active = true;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(userType.name()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.active;
+    }
+
+    public boolean hasRole(String userType) {
+        return getAuthorities().contains(new SimpleGrantedAuthority(userType));
+    }
 }
