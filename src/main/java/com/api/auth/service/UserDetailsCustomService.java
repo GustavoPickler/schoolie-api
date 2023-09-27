a373fb05c1af93cad3db02a0d3fe627f753653d2
Gabriel Pickler <gpickler98@gmail.com>
2023-09-27 18:16:40 -0300

    feat(UserDetailsCustomService): implementa spring security UserDetailsService

---
+++ src/main/java/com/api/auth/service/UserDetailsCustomService.java
@@ -0,0 +1,26 @@
+package com.api.auth.service;
+
+import com.api.users.repository.UserRepository;
+import com.api.users.utils.ErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Getter
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class UserDetailsCustomService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException(ErrorCode.USER_NOT_FOUND.getMessage()));
+    }
+}
